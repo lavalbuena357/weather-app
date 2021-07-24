61ac7d75ce40c2aa027749dfbbd2a9277f08ce54
albertvr357 <cuervo357@gmail.com>
2021-07-24 13:55:36 -0500

    main module updates

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react'
+import { connect } from 'react-redux'
+import { getCity } from '../../redux/actions'
+import style from './Main.module.css'
+import { FaSearch } from "react-icons/fa";
+import Cards from '../Cards/Cards'
+
+function Main({ getCity }) {
+  const [cities, setCities] = useState([])
+
+  let cityStr = '' 
+
+  function clear() {
+    let textInput = document.getElementById('search')
+    if(textInput !== null) {
+      textInput.value = ''
+    }
+  }
+  
+  function handleChange(e) {
+    cityStr = e.target.value.toLowerCase()
+  }
+  
+  function handleSearch(e) {
+    e.preventDefault()
+    if(!cities.includes(cityStr)) {
+      setCities([...cities, cityStr])
+      getCity(cityStr)
+    }
+    clear()
+  }
+
+  return (
+    <div className={style.container}>
+      <div className={style.search_ctn}>
+        <form onSubmit={(e) => handleSearch(e)}>
+          <FaSearch />
+          <input type="search" placeholder='Search city...' id='search' onChange={(e) => handleChange(e)} />
+          <input type="submit" value='Search' />
+        </form>
+      </div>
+      <div className={style.content}>
+        <Cards />
+      </div>
+    </div>
+  )
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    getCity: (data) => dispatch(getCity(data))
+  }
+}
+
+export default connect(null, mapDispatchToProps)(Main);
