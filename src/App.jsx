c862d6ea47ed38168099614aa0052ded7e7a9ea9
albertvr357 <cuervo357@gmail.com>
2021-07-23 22:45:06 -0500

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import { Route } from 'react-router-dom';
 import './App.css'
+import Main from './components/Main/Main'
 
 function App() {
 
   return (
     <div className="App">
-      Weather App
+      {/* <Route  path='/' component={Header} /> */}
+      {/* <Route exact path='/' component={Search} /> */}
+      <Route exact path='/' component={Main} />
+      {/* <Route exact path='/detail/:name' component={Detail} /> */}
     </div>
   )
 }
