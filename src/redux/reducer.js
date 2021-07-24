6572490a101a1132900353a733ac19eadaefbbab
albertvr357 <cuervo357@gmail.com>
2021-07-24 13:53:52 -0500

    reducer fix

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -8,7 +8,7 @@ function reducer(state = initialState, action) {
   switch(action.type) {
     case GET_CITY:
       return {...state,
-        city: action.payload
+        city: [...state.city, action.payload]
       }
     default:
       return state
