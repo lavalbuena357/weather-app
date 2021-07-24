044d6ed24887255328b497ea44810879d0ee7b77
albertvr357 <cuervo357@gmail.com>
2021-07-24 13:55:59 -0500

    cards module updates

---
+++ src/components/Cards/Cards.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import style from './Cards.module.css'
+import { connect } from 'react-redux'
+import Card from '../Card/Card'
+
+function Cards({ city, error }) {
+  return (
+    <div className={style.container}>
+      {city.map((el, i) => 
+        <Card 
+          key={i}
+          name={el.name}
+          country={el.sys.country}
+          temp={el.main.temp}
+          min={el.main.temp_min}
+          max={el.main.temp_max}
+          icon={el.weather[0].icon}
+          weather={el.weather[0].main}
+        />
+      )}
+    </div>
+  )
+}
+
+function mapStateToProps(state) {
+  return {
+    city: state.city,
+    error: state.error
+  }
+}
+
+export default connect(mapStateToProps)(Cards);
