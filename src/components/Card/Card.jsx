9cd74dac8062bdcb48605c63c1cd327502a60abe
albertvr357 <cuervo357@gmail.com>
2021-07-24 18:31:25 -0500

    card module update

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -14,22 +14,22 @@ function Card({ name, country, min, max, icon, temp, weather, id, updateCities }
   return (
     <div className={style.container}>
       <div className={style.btn}>
-        <IoIosCloseCircle onClick={onClose} />
+        <IoIosCloseCircle className={style.close} onClick={onClose} />
       </div>
       <div className={style.head_ctn}>
-        <ImLocation2 />
-        <h1>{name}</h1>
+        <ImLocation2 className={style.location} />
+        <h2>{name}</h2>
         <h3>{country}</h3>
       </div>
       <div className={style.temp}>
-        <span>{parseInt(temp)}°</span>
-        <div className={style.min_max_icon}>
-          <img src={`http://openweathermap.org/img/wn/${icon}@2x.png`} alt="" />
-          <div className={style.min_max}>
-            <span>Max: {max}°</span>
-            <span>Min: {min}°</span>
-          </div>
+        <h2>{parseInt(temp)}°</h2>
+        <div className={style.min_max}>
+          <span>Max: {max}°</span>
+          <span>Min: {min}°</span>
         </div>
+      </div>
+      <div className={style.icon}>
+         <img src={`http://openweathermap.org/img/wn/${icon}@2x.png`} alt="" />
       </div>
       <div className={style.weather}>
         <p>{weather}</p>
