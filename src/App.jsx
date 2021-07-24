9139e0e5b167931435017cf88fe73246b0a6ec9d
albertvr357 <cuervo357@gmail.com>
2021-07-24 18:34:59 -0500

    app update routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,14 @@ import React from 'react'
 import { Route } from 'react-router-dom';
 import './App.css'
 import Main from './components/Main/Main'
+import Detail from './components/Detail/Detail'
 
 function App() {
 
   return (
     <div className="App">
-      {/* <Route  path='/' component={Header} /> */}
-      {/* <Route exact path='/' component={Search} /> */}
       <Route exact path='/' component={Main} />
-      {/* <Route exact path='/detail/:name' component={Detail} /> */}
+      <Route exact path='/detail/:id' component={Detail} />
     </div>
   )
 }
