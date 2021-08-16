92d97b3619c8b236e696998d33a27e0d7205ed0a
cuervo357@gmail.com <cuervo357@gmail.com>
2021-08-16 13:43:39 -0500

    remove routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Route } from 'react-router-dom';
 import './App.css'
 import Header from './components/Header/Header';
 import Main from './components/Main/Main'
@@ -9,9 +8,7 @@ function App() {
 
   return (
     <div className="App">
-      <Route path='/' component={Header} />
-      <Route exact path='/' component={Main} />
-      <Route exact path='/detail/:id' component={Detail} />
+      <Header/>
     </div>
   )
 }
