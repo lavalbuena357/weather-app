bee5ff084f26756bc3c2d403f4edb10a28cfff81
cuervo357@gmail.com <cuervo357@gmail.com>
2021-08-16 20:38:38 -0500

    new module

---
+++ src/components/FirstCard/FirstCard.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import style from './FirstCard.module.css'
+import { connect } from 'react-redux'
+import { ImLocation2 } from 'react-icons/im';
+import { IoIosCloseCircle } from "react-icons/io";
+import { updateCities } from '../../redux/actions';
+
+function FirstCard({ name, country, min, max, icon, temp, weather, id, updateCities, handleDetails }) {
+
+  function onClose() {
+    updateCities(id)
+  }
+
+  return (
+    <div className={style.container}>
+      <div className={style.btn}>
+        <IoIosCloseCircle className={style.close} onClick={onClose} />
+      </div>
+      <div onClick={() => handleDetails(id)} className={style.head_ctn}>
+        <ImLocation2 className={style.location} />
+        <h2>{name}</h2>
+        <h3>{country}</h3>
+      </div>
+      <div onClick={() => handleDetails(id)} className={style.temp}>
+        <h2>{parseInt(temp)}°</h2>
+        <div className={style.min_max}>
+          <span>Max: {max}°</span>
+          <span>Min: {min}°</span>
+        </div>
+      </div>
+      <div onClick={() => handleDetails(id)} className={style.icon}>
+        <img src={`http://openweathermap.org/img/wn/${icon}@2x.png`} alt="" />
+      </div>
+      <div className={style.weather}>
+        <p>{weather}</p>
+      </div>
+  </div>
+  )
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    updateCities: (data) => dispatch(updateCities(data))
+  }
+}
+
+export default connect(null, mapDispatchToProps)(FirstCard);
