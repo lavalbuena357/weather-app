b3a8effeee48bc12cdd82ee5ed70ca132d557f68
cuervo357@gmail.com <cuervo357@gmail.com>
2021-08-16 20:39:33 -0500

    update

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -4,9 +4,8 @@ import style from './Card.module.css'
 import { ImLocation2 } from 'react-icons/im';
 import { IoIosCloseCircle } from "react-icons/io";
 import { updateCities } from '../../redux/actions';
-import { Link } from 'react-router-dom';
 
-function Card({ name, country, min, max, icon, temp, weather, id, updateCities }) {
+function Card({ name, country, min, max, icon, temp, weather, id, updateCities, handleDetails }) {
 
   function onClose() {
     updateCities(id)
@@ -14,29 +13,27 @@ function Card({ name, country, min, max, icon, temp, weather, id, updateCities }
 
   return (
     <div className={style.container}>
-      <Link to={`/detail/${id}`}>
         <div className={style.btn}>
           <IoIosCloseCircle className={style.close} onClick={onClose} />
         </div>
-        <div className={style.head_ctn}>
+        <div onClick={() => handleDetails(id)} className={style.head_ctn}>
           <ImLocation2 className={style.location} />
           <h2>{name}</h2>
           <h3>{country}</h3>
         </div>
-        <div className={style.temp}>
+        <div onClick={() => handleDetails(id)} className={style.temp}>
           <h2>{parseInt(temp)}°</h2>
           <div className={style.min_max}>
             <span>Max: {max}°</span>
             <span>Min: {min}°</span>
           </div>
         </div>
-        <div className={style.icon}>
+        <div onClick={() => handleDetails(id)} className={style.icon}>
           <img src={`http://openweathermap.org/img/wn/${icon}@2x.png`} alt="" />
         </div>
         <div className={style.weather}>
           <p>{weather}</p>
         </div>
-      </Link>
     </div>
   )
 }
